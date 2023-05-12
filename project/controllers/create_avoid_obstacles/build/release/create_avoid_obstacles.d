f258ec69e143665d27b513fa1d5d258ad7134b65
madii303 <115805362+madii303@users.noreply.github.com>
2023-05-12 15:30:44 +0500

    Add files via upload

---
+++ project/controllers/create_avoid_obstacles/build/release/create_avoid_obstacles.d
@@ -0,0 +1,10 @@
+build/release/create_avoid_obstacles.o: create_avoid_obstacles.c \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/led.h \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/position_sensor.h \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/receiver.h \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\Madiha\AppData\Local\Programs\Webots/include/controller/c/webots/touch_sensor.h
